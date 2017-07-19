fd492c16e649e51300de5805328b37486a47019e
RJ.HWang <rongjihuang@gmail.com>
2017-07-19 18:33:47 +0800

    Refactor JaxrsTestConfiguration to load all Components bellow 'tech.simter.genson.ext' and 'tech.simter.rest.jaxrs' packages

--- src/main/java/tech/simter/test/JaxrsTestConfiguration.java
+++ src/main/java/tech/simter/test/JaxrsTestConfiguration.java
@@ -1,36 +1,12 @@
 package tech.simter.test;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import tech.simter.genson.ext.data.PageConverterFactory;
-import tech.simter.rest.jaxrs.CreatedStatusResponseFilter;
-import tech.simter.rest.jaxrs.jersey.JerseyConfiguration;
-import tech.simter.rest.jaxrs.jersey.JerseyResourceConfig;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author RJ
  */
 @TestConfiguration
+@ComponentScan(basePackages = {"tech.simter.genson.ext", "tech.simter.rest.jaxrs"})
 public class JaxrsTestConfiguration {
-  @Bean
-  @ConfigurationProperties(prefix = "simter.jersey")
-  JerseyConfiguration jerseyConfiguration() {
-    return new JerseyConfiguration();
-  }
-
-  @Bean
-  JerseyResourceConfig jerseyResourceConfig() {
-    return new JerseyResourceConfig();
-  }
-
-  @Bean
-  CreatedStatusResponseFilter createdStatusResponseFilter() {
-    return new CreatedStatusResponseFilter();
-  }
-
-  @Bean
-  PageConverterFactory pageConverterFactory() {
-    return new PageConverterFactory();
-  }
 }
